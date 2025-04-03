7b3ac684d6d5da67d28a1ecd79904b2e09a4d1f1
XINDQ <xdqnd2012@163.com>
2025-04-04 00:07:02 +0800

    异常类

---
+++ qlang-core/src/main/java/com/github/qlang/core/exception/QLangException.java
@@ -0,0 +1,22 @@
+package com.github.qlang.core.exception;
+
+public class QLangException extends RuntimeException {
+    public QLangException() {
+    }
+
+    public QLangException(String message) {
+        super(message);
+    }
+
+    public QLangException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QLangException(Throwable cause) {
+        super(cause);
+    }
+
+    public QLangException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
